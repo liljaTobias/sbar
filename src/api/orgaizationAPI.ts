341d0eddedd17f07a9f28121d8c4a80f6f6b5655
Tobias Lilja <lilja.tobias94@gmail.com>
2021-09-27 17:09:50 +0200

    fix: Subcategory list toggle

--- src/api/orgaizationAPI.ts
+++ src/api/orgaizationAPI.ts
@@ -1,11 +1,32 @@
-import { useQuery, UseQueryResult } from "react-query"
-import { Organization } from "../components/List/types"
+import { useQuery, UseQueryResult } from 'react-query'
+
+export interface SubCategory {
+    subcategory_id: string
+    subcategory_name: string
+    actions: Map<string, string>
+}
+
+export interface Category {
+    category_id: string
+    category_name: string
+    subcategories: Array<SubCategory>
+}
+
+export interface Organization {
+    organization_id: string
+    organization_name: string
+    categories: Array<Category>
+}
 
 export const useOrganization = (organization_id: string): UseQueryResult<Organization, Error> => {
-    return useQuery<Organization, Error>("organization", async () => {
-        const BASE_URL = 'https://t1vy4habx7.execute-api.eu-north-1.amazonaws.com/organizations'
-        const res = await fetch(`${BASE_URL}/${organization_id}`)
-        const data = await res.json()
-        return data.Item
-    }, { refetchOnWindowFocus: false })
+    return useQuery<Organization, Error>(
+        'organization',
+        async () => {
+            const BASE_URL = 'https://t1vy4habx7.execute-api.eu-north-1.amazonaws.com/organizations'
+            const res = await fetch(`${BASE_URL}/${organization_id}`)
+            const data = await res.json()
+            return data.Item
+        },
+        { refetchOnWindowFocus: false },
+    )
 }
--- src/components/Dashboard/Dashboard.style.ts
+++ src/components/Dashboard/Dashboard.style.ts
@@ -1,9 +1,18 @@
 import { makeStyles, Theme, createStyles } from '@material-ui/core'
 
-const useStyles = makeStyles(({ spacing }: Theme) =>
+const useStyles = makeStyles(({ spacing, palette }: Theme) =>
     createStyles({
         container: {
             marginTop: spacing(5),
+        },
+        subCategoryList: {
+            backgroundColor: palette.subCategoryBg.main,
+            '&:focus': {
+                backgroundColor: palette.subCategoryBg.main,
+            },
+        },
+        actionList: {
+            backgroundColor: palette.grey[100],
         },
     }),
 )
